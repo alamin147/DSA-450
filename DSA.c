c5c88e16b01890a21ed9b9e2c6c1c8be2ddc029b
alamin.147 <alamin.14780@gmail.com>
2023-08-16 16:31:54 +0600

    updated

--- DSA.c
+++ DSA.c
@@ -119,6 +119,7 @@
 
 // 4 Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
 // I use two array. one copied worked then paste it. Output 0,1,2 only. I initially made all element 1 then replace with 2 or 0 if found.
+// input: [1 2 0 2 1 0 2] output: [0 0 1 1 2 2 2]
 // #include <stdio.h>
 // int main()
 // {
@@ -153,7 +154,38 @@
 //     }
 //     for (int i = 0; i < n; i++)
 //     { a[i] = b[i]; }
-    
+
 //     for (int i = 0; i < n; i++)
-//     { printf("%d ",a[i]);}  
+//     { printf("%d ",a[i]);}
 // }
+
+// ###################################################################################################
+// 5. Move all the negative elements to one side of the array
+// Input: -12, 11, -13, -5, 6, -7, 5, -3, -6 Output: -12 -13 -5 -7 -3 -6 11 6 5
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int a[n], b[n], j = 0, k = n - 1;
+//     for (int i = 0; i < n; i++)
+//     { scanf("%d", &a[i]);  }
+
+//     for (int i = 0; i < n; i++)
+//     { // if found negative then element is moved to new array's starting positions and so on.
+//         if (a[i] < 0)
+//         {  b[j] = a[i];
+//             j++; }
+//         // positive values will be moved to the end of array then come back to start
+//         else
+//         { b[k] = a[i];
+//             k--; }
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {printf("%d ", b[i]); }}
+
+//###################################################################################################
+
+//6. 
